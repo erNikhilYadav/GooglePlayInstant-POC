b30668951f4c7bfc40af830251e7ad2d8db20ff8
Nikhil <nikhil.yadav@carnot.co.in>
2024-04-01 19:37:50 +0530

    Updated GitIgnore | Added Deeplink to Launch Instant App | Added Install App from PS

--- app/src/main/java/com/nikhilyadav/googleplayinstant/MainActivity.kt
+++ app/src/main/java/com/nikhilyadav/googleplayinstant/MainActivity.kt
@@ -1,12 +1,17 @@
 package com.nikhilyadav.googleplayinstant
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
@@ -20,13 +25,19 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.google.android.gms.instantapps.InstantApps
 import com.nikhilyadav.googleplayinstant.ui.theme.GooglePlayInstantTheme
 
 class MainActivity : ComponentActivity() {
 
-    val quotes = listOf(
+    /*
+        Dummy Data to display something on screen
+    */
+    private val quotes = listOf(
         "The only way to do great work is to love what you do.",
         "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
         "Programs must be written for people to read, and only incidentally for machines to execute.",
@@ -39,49 +50,131 @@ class MainActivity : ComponentActivity() {
         "Walking on water and developing software from a specification are easy if both are frozen."
     )
 
+    /**
+     * Intent to launch after the app has been installed.
+     */
+    private val postInstallIntent = Intent(
+        Intent.ACTION_VIEW,
+        Uri.parse("https://instant-app-poc.com/")
+    ).addCategory(Intent.CATEGORY_BROWSABLE).putExtras(Bundle().apply {
+        putString("The key to", "sending data via intent")
+    })
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val isInstantApp = InstantApps.getPackageManagerCompat(this).isInstantApp
         setContent {
             GooglePlayInstantTheme {
                 var currentQuote by remember { mutableStateOf(quotes.random()) }
                 Surface(
                     modifier = Modifier
                         .fillMaxSize()
-                        .padding(16.dp),
-                    color = MaterialTheme.colorScheme.background
                 ) {
                     Column(
-                        verticalArrangement = Arrangement.Center,
+                        verticalArrangement = Arrangement.SpaceEvenly,
                         horizontalAlignment = Alignment.CenterHorizontally,
                     ) {
-                        Greeting(currentQuote)
-                        Spacer(modifier = Modifier.height(16.dp))
-                        Button(onClick = {
-                            currentQuote = quotes.random()
-                        }) {
-                            Text("Click to get Random Quote")
+
+                        Row(
+                            horizontalArrangement = Arrangement.End,
+                            verticalAlignment = Alignment.CenterVertically,
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(16.dp)
+                        ) {
+                            Text(text = "Running As: ", fontWeight = FontWeight.Bold)
+                            Text(text = if (isInstantApp) "Instant App" else "Main App")
                         }
+
+                        Quote(currentQuote)
+
+                        Spacer(modifier = Modifier.height(16.dp))
+
+                        Row(
+                            horizontalArrangement = Arrangement.SpaceEvenly,
+                            verticalAlignment = Alignment.CenterVertically,
+                            modifier = Modifier.fillMaxWidth()
+                        ) {
+                            Button(onClick = {
+                                currentQuote = quotes.random()
+                            }) {
+                                Text("Get New")
+                            }
+                            Button(
+                                enabled = isInstantApp,
+                                onClick = {
+                                    if (isInstantApp) {
+                                        InstantApps.showInstallPrompt(
+                                            this@MainActivity,
+                                            postInstallIntent,
+                                            REQUEST_CODE,
+                                            REFERRER
+                                        )
+                                    } else {
+                                        Toast.makeText(
+                                            this@MainActivity,
+                                            "Check Settings to deploy as Sample",
+                                            Toast.LENGTH_SHORT
+                                        ).show()
+                                    }
+                                }) {
+                                Text(text = "Install App")
+                            }
+                        }
+
                     }
                 }
             }
         }
     }
+
+    companion object {
+        private val REFERRER = "MainActivity"
+        private val REQUEST_CODE = 7
+    }
+
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
+fun Quote(quote: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello $name!",
-        modifier = modifier,
+        text = quote,
+        modifier = modifier.padding(16.dp),
         style = MaterialTheme.typography.headlineLarge
     )
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun QuotePreview() {
     GooglePlayInstantTheme {
-        Greeting("Android")
+        Surface(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp),
+            color = Color.White
+        ) {
+            Column(
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally,
+            ) {
+                Quote("The only way to do great work is to love what you do.")
+                Spacer(modifier = Modifier.height(16.dp))
+
+                Row(
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Button(onClick = {
+                    }) {
+                        Text("Get New")
+                    }
+                    Button(onClick = { /*TODO*/ }) {
+                        Text("Install App")
+                    }
+                }
+
+            }
+        }
     }
 }
